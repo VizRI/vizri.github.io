a26576974bb692d198b24f073d0e074cf4d24ce3
Alex <Smirkyredpanda@gmail.com>
2022-08-18 07:37:00 -0400

    Update documentation

---
+++ campaign-finance/nuxt/state-65f968f9.mjs
@@ -0,0 +1,1 @@
+import{y as t,z as e}from"./entry-d3931f78.mjs";const s=()=>{const r=t();return e("quarter",()=>r.public.quarter,"$EslRDM4Dc8")};export{s as c};
--- campfi-test/nuxt/state-9f62f7c7.mjs
+++
@@ -1,1 +0,0 @@
-import{x as t,y as e}from"./entry-21f8eecd.mjs";const s=()=>{const r=t();return e("quarter",()=>r.public.quarter,"$EslRDM4Dc8")};export{s as c};
