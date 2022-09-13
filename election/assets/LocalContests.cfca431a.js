202db3b07f92a5caa897d5440b8315ab783449f9
Alex <Smirkyredpanda@gmail.com>
2022-09-13 06:27:18 -0400

    Update documentation

--- election/assets/LocalContests.cfca431a.js
+++
@@ -1,1 +0,0 @@
-import{u as o,o as s,c,h as a,T as r}from"./index.1bab5c39.js";const m={__name:"LocalContests",setup(n){const e=o(),t=e.params.id.replace("_"," ");return(l,u)=>(s(),c(r,{electionName:a(t),fileURL:a(e).params.id},null,8,["electionName","fileURL"]))}};export{m as default};
