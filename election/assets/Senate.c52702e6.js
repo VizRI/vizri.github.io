6aa169eebd4a535eb2fce5c1b6d0b997eafaa1de
Alex <Smirkyredpanda@gmail.com>
2022-09-13 08:42:16 -0400

    Update documentation

--- election/assets/Senate.c52702e6.js
+++
@@ -1,1 +0,0 @@
-import{o as e,c as t,T as a}from"./index.1c0950b7.js";const r={__name:"Senate",setup(o){return(s,c)=>(e(),t(a,{electionName:"State Senators",fileURL:"state_senate"}))}};export{r as default};
---
+++ election/assets/Senate.e4ab41c9.js
@@ -0,0 +1,1 @@
+import{o as e,c as t,T as a}from"./index.e063f06b.js";const r={__name:"Senate",setup(o){return(s,c)=>(e(),t(a,{electionName:"State Senators",fileURL:"state_senate"}))}};export{r as default};
