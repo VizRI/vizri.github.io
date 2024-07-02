247486ea438f16afa5275c03bddb6d9ffece7699
Alex <Smirkyredpanda@gmail.com>
2024-07-01 20:28:33 -0400

    Update documentation

---
+++ election-test/assets/House-CoS9W_Xo.js
@@ -0,0 +1,1 @@
+import{c as e,o as t,T as a}from"./index-C9s4TIqk.js";const _={__name:"House",setup(o){return(s,c)=>(t(),e(a,{electionName:"State Represenatives",fileURL:"state_house"}))}};export{_ as default};
