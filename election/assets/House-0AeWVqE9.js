f1c500ca357f05ba5f26e525ffee6b3c48b4bb1b
Alex <Smirkyredpanda@gmail.com>
2024-09-10 17:39:36 -0400

    Update documentation

---
+++ election/assets/House-0AeWVqE9.js
@@ -0,0 +1,1 @@
+import{c as e,o as t,T as a}from"./index-AXn9W-n9.js";const _={__name:"House",setup(o){return(s,c)=>(t(),e(a,{electionName:"State Represenatives",fileURL:"state_house"}))}};export{_ as default};
---
+++ election/assets/Senate-BQUv1FUg.js
@@ -0,0 +1,1 @@
+import{c as e,o as t,T as a}from"./index-AXn9W-n9.js";const r={__name:"Senate",setup(o){return(s,c)=>(t(),e(a,{electionName:"State Senators",fileURL:"state_senate"}))}};export{r as default};
