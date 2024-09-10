45945d843b1d66aea5789d5da1f8ab7c5f343484
Alex <Smirkyredpanda@gmail.com>
2024-09-10 10:13:36 -0400

    Update documentation

---
+++ election-test/assets/Senate-BAYdJ0US.js
@@ -0,0 +1,1 @@
+import{c as e,o as t,T as a}from"./index-C9s4TIqk.js";const r={__name:"Senate",setup(o){return(s,c)=>(t(),e(a,{electionName:"State Senators",fileURL:"state_senate"}))}};export{r as default};
