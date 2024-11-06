1c748cb1b87d0e5006455d4cebce6bbb580fc251
Alex <Smirkyredpanda@gmail.com>
2024-11-06 16:40:28 -0500

    Update documentation

---
+++ election/assets/Senate-B26TvIVF.js
@@ -0,0 +1,1 @@
+import{c as e,o as t,T as a}from"./index-BlGRqcNw.js";const r={__name:"Senate",setup(o){return(s,c)=>(t(),e(a,{electionName:"State Senators",fileURL:"state_senate"}))}};export{r as default};
